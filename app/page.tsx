cce8bdb9406abde2ff206cc8058fa1b724c7e3d9
lilKriT <lilKriTwebDev@gmail.com>
2026-02-08 06:58:01 +0100

    Styling. Structure.

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,64 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="">
-      <p>Social Media 10x Faster with AI</p>
-      <p>Over 4,000 5-star reviews</p>
+    <section className="min-h-dvh flex justify-center items-center">
+      <div className="grid grid-cols-12 grid-rows-12 gap-8 mx-8 my-32">
+        {/* First section */}
+        <div className="flex flex-col justify-center items-center col-start-4 row-start-1 col-span-6 row-span-4 bg-purple-500 text-white p-8 text-center rounded-xl">
+          <h2 className="text-5xl">
+            Social Media <span className="text-yellow-500">10x</span>{" "}
+            <i>Faster</i> with AI
+          </h2>
+          <Image
+            src={"/illustration-five-stars.webp"}
+            alt="5 star rating"
+            width={384}
+            height={64}
+            className="h-8 w-auto"
+          />
+          <p>Over 4,000 5-star reviews</p>
+        </div>
 
-      <p>Manage multiple accounts and platforms.</p>
+        {/* Second section */}
+        <div className="flex flex-col justify-center items-center col-start-4 row-start-5 col-span-3 row-span-4 bg-white rounded-xl">
+          <p>Manage multiple accounts and platforms.</p>
+        </div>
 
-      <p>Maintain a consistent posting schedule.</p>
+        {/* Third section */}
+        <div className="flex flex-col justify-center items-center col-start-7 row-start-5 col-span-3 row-span-4 bg-yellow-500 rounded-xl">
+          <p>Maintain a consistent posting schedule.</p>
+        </div>
 
-      <p>Schedule to social media.</p>
-      <p>
-        Optimize post timings to publish content at the perfect time for your
-        audience.
-      </p>
+        {/* Fourth section */}
+        <div className="flex flex-col justify-center items-center col-start-10 row-start-1 col-span-3 row-span-8 bg-purple-100 rounded-xl">
+          <p>Schedule to social media.</p>
+          <p>
+            Optimize post timings to publish content at the perfect time for
+            your audience.
+          </p>
+        </div>
 
-      <p>Grow followers with non-stop content.</p>
+        {/* Fifth section */}
+        <div className="flex flex-col justify-center items-center col-start-7 row-start-9 col-span-6 row-span-4 bg-purple-500 rounded-xl">
+          <p>Grow followers with non-stop content.</p>
+        </div>
 
-      <p>{">56% faster audience growth"}</p>
+        {/* Sixth section */}
+        <div className="flex flex-col justify-center items-center col-start-4 row-start-9 col-span-3 row-span-4 bg-white rounded-xl">
+          <p>{">56% faster audience growth"}</p>
+        </div>
 
-      <p>Create and schedule content quicker.</p>
+        {/* Seventh section */}
+        <div className="flex flex-col justify-center items-center col-start-1 row-start-1 col-span-3 row-span-6 bg-yellow-100 rounded-xl">
+          <p>Create and schedule content quicker.</p>
+        </div>
 
-      <p>Write your content using AI.</p>
-    </div>
+        {/* Eigth section */}
+        <div className="flex flex-col justify-center items-center col-start-1 row-start-7 col-span-3 row-span-6 bg-yellow-500 rounded-xl">
+          <p>Write your content using AI.</p>
+        </div>
+      </div>
+    </section>
   );
 }
