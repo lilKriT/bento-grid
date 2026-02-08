f11dcee7569b222bb2b7da6bf99dfed0623ca031
lilKriT <lilKriTwebDev@gmail.com>
2026-02-08 21:42:34 +0100

    Mobile done.

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const defaultStyle = "flex flex-col justify-center items-center rounded-xl ";
 export default function Home() {
   return (
     <section className="min-h-dvh flex justify-center items-center">
-      <div className="max-w-7xl grid grid-cols-12 grid-rows-[repeat(12,auto)] gap-8 mx-8 my-16">
+      <div className="max-w-7xl flex flex-col gap-8 lg:grid grid-cols-12 grid-rows-[repeat(12,auto)] gap-8 mx-8 my-16">
         {/* First section */}
         <div
           className={
@@ -31,7 +31,7 @@ export default function Home() {
         <div
           className={
             defaultStyle +
-            "col-start-4 row-start-5 col-span-3 row-span-4 bg-white p-6 overflow-hidden"
+            "col-start-4 row-start-5 col-span-3 row-span-4 bg-white p-6 overflow-hidden flex flex-col gap-4"
           }
         >
           <Image
@@ -39,9 +39,9 @@ export default function Home() {
             alt="Multiple platforms"
             width={633}
             height={134}
-            className="scale-125 origin-left rounded-full drop-shadow-2xl drop-shadow-white"
+            className="scale-125 origin-left rounded-full"
           />
-          <p className="text-4xl font-medium leading-none mt-auto">
+          <p className="text-xl md:text-2xl lg:text-4xl font-medium leading-none mt-auto">
             Manage multiple accounts and platforms.
           </p>
         </div>
@@ -50,7 +50,7 @@ export default function Home() {
         <div
           className={
             defaultStyle +
-            "col-start-7 row-start-5 col-span-3 row-span-4 bg-yellow-500 p-8 pb-0 overflow-hidden gap-4"
+            "col-start-7 row-start-5 col-span-3 row-span-4 bg-yellow-500 pl-4 pr-20 pt-4 lg:p-8 pb-0 overflow-hidden gap-4"
           }
         >
           <p className="text-4xl font-medium leading-none text-balance">
@@ -72,7 +72,7 @@ export default function Home() {
             "col-start-10 row-start-1 col-span-3 row-span-8 bg-purple-100 gap-8 overflow-hidden py-8"
           }
         >
-          <p className="text-3xl font-medium leading-none text-balance px-8">
+          <p className="text-2xl lg:text-3xl font-medium leading-none text-balance px-8">
             Schedule to social media.
           </p>
           <Image
@@ -80,7 +80,7 @@ export default function Home() {
             alt="Post Scheduling"
             width={715}
             height={638}
-            className="ml-16 my-4 origin-left scale-130"
+            className="px-4 lg:px-0 lg:ml-16 my-4 origin-left lg:scale-130"
           />
           <p className="font-medium px-8">
             Optimize post timings to publish content at the perfect time for
@@ -92,7 +92,7 @@ export default function Home() {
         <div
           className={
             defaultStyle +
-            "col-start-7 row-start-9 col-span-6 row-span-4 grid grid-cols-2 bg-purple-500 p-6 gap-4"
+            "col-start-7 row-start-9 col-span-6 row-span-4 flex flex-col gap-8 px-12 py-6 lg:grid grid-cols-2 bg-purple-500 lg:p-6 lg:gap-4"
           }
         >
           <Image
@@ -150,10 +150,10 @@ export default function Home() {
         <div
           className={
             defaultStyle +
-            "col-start-1 row-start-7 col-span-3 row-span-6 bg-yellow-500 p-8"
+            "col-start-1 row-start-7 col-span-3 row-span-6 bg-yellow-500 p-8 gap-8 items-start lg:items-center"
           }
         >
-          <p className="text-4xl text-balance font-medium leading-none">
+          <p className="text-3xl lg:text-4xl text-balance font-medium leading-none">
             Write your content using AI.
           </p>
           <Image
@@ -161,7 +161,7 @@ export default function Home() {
             alt="Write your content..."
             width={440}
             height={446}
-            className="mt-auto scale-105 origin-left"
+            className="mt-auto lg:scale-105 origin-left"
           />
         </div>
       </div>
